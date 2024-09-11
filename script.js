f688b927eeb5216a400d5ea6a8ad3cf17aa9b264
han12312345 <135451905+han12312345@users.noreply.github.com>
2024-09-11 23:15:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const button = document.querySelector("button");
+
+function displayLifeHabits() {
+    alert("法斗的生活习性包括：温和的性格、适量的活动需求、较强的环境适应能力、定期的护理需求、注意健康状况、训练时需要耐心和一致性。");
+}
+
+button.addEventListener("click", displayLifeHabits);
